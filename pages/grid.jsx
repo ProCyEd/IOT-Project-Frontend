cf553308c63a827bfc1a03d29380c7d2b84ca111
shelleyyyyy <shelleywr23@mail.vmi.edu>
2021-11-16 07:45:38 -0500

    IOT-5 #time 30m

---
+++ pages/grid.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Contaienr from '@mui/material/Container';
+import style from '../styles/grid.module.scss';
+
+export default function ExampleGrid(){
+    return(
+        <Contaienr>
+            <Grid display="flex" container spacing={3}>
+                <Grid item xs={8}>
+                    <Paper
+                        sx={{
+                        p: 1,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        height: 250,
+                        }}
+                    >
+
+                    </Paper>
+                </Grid>
+                <Grid item xs={4}>
+                    <Paper
+                        sx={{
+                        p: 1,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        height: 250,
+                        }}
+                    >
+
+                    </Paper>
+                </Grid>
+                <Grid item xs={6}>
+                    <Paper
+                        sx={{
+                        p: 1,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        height: 250,
+                        }}
+                    >
+                    </Paper>
+                </Grid>
+                <Grid item xs={6}>
+                    <Paper
+                        sx={{
+                        p: 1,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        height: 250,
+                        }}
+                    >
+                    </Paper>
+                </Grid>
+            </Grid>
+        </Contaienr>
+    )
+}
