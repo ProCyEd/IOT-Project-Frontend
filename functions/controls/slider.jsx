88c7f1f88b1e4f253e1fef2b84db5bc90364d0f1
BoleBorson <corsonca23@mail.vmi.edu>
2022-01-30 19:23:29 -0500

    color picker

--- functions/controls/slider.jsx
+++ functions/controls/slider.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Slider from '@mui/material/Slider'
 
 export default function Mover({name}) {
+
+    const [brightness, setBrightness] = useState(50);
+
+    function handleChange(event, newValue) {
+        setBrightness(newValue)
+    }
+
     return <>
         <div>{name}</div>
-        <Slider defaultValue={50} aria-label="Default" valueLabelDisplay="auto" />
+        <Slider value={brightness} onChange={handleChange} aria-label="Default" valueLabelDisplay="auto" />
     </> 
   
 }
