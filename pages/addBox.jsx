c31fd1b696c97ab5e5e708372e813594b7390dde
aidan-noonan <73564751+aidan-noonan@users.noreply.github.com>
2022-03-27 01:48:15 -0400

    test

--- pages/addBox.jsx
+++ pages/addBox.jsx
@@ -1,74 +1,79 @@
 import React from 'react';
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Container from '@mui/material/Container';
+import TopBar from '../components/dashboard/topBar';
+import Copyright from '../components/copyright';
+import withAuth from '../components/auth/checkPerms';
+import GettingStarted from '../components/dashboard/gettingStarted';
+import { default as NextLink } from 'next/link'
 
-function GettingStarted() {
-
-    async function reserve() {
-
-        const data = {
-            message: "Testing"
-        }
-
-        await fetch('http://localhost:3001/control/publish', {
-        method: 'POST',
-        credentials: 'include',
-        headers: {
-            'Content-Type': 'application/json',
-            /* 'Access-Control-Allow-Origin': 'http://localhost:3001/api/reservation',
-            'Access-Control-Allow-Credentials': true, */
+const mdTheme = createTheme({
+    palette: {
+        primary: {
+          main: '#FF0000',
         },
-            body: JSON.stringify(data),
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-        })  
-        .catch((error) => {
-            console.error('Error:', error);
-        });
-        }
+    }
+  });
+
+function addBox() {
 
 
+  
   return (
     <div>
         <Grid container spacing={2}>
-            <Grid item xs={8}>
+            <Grid item xs={10}>
                 <h1>Add a box</h1>
             </Grid>
-            <Grid item xs={6}>
+            <Grid item xs={8}>
                 <p>
                 
                     Box Name: 
                     <br></br>
-                    <input type="Text" name="boxName" id = "boxName" /> 
+                    <input type="Text" name="boxName" id = "boxName"  /> 
                     <br></br><br></br>
 
                     Box Location 
                     <br></br>
                     <input type="Text" name="boxLocation" id = "boxLocation" /> 
                     <br></br>
+                    <br></br>
 
-                   
-
-
+                    <Grid item xs={6}>
+                        <h1>Add device to box</h1>
+                    </Grid>
+                    <br></br>
+                    <br></br>
+                    <input type="checkbox" id="lightCheck" Accept/> Light IP: 
+                    <input type="Text" name="lightText" id = "lightText" /> 
+                    <br></br>
+                    <br></br>
+                    <input type="checkbox" id="webcamCheck" Accept/> Webcam IP: 
+                    <input type="Text" name="webcamText" id = "webcamText" /> 
+                    <br></br>
+                    <br></br>
+                    <input type="checkbox" id="smartoutletCheck" Accept/> Smart outlet IP: 
+                    <input type="Text" name="smartoutletText" id = "smartoutletText" /> 
+                    <br></br>
+                    <br></br>
+                    <input type="checkbox" id="smartStripCheck" Accept/> Smart strip IP: 
+                    <input type="Text" name="smartStripText" id = "smartStripText" /> 
                 </p>
             </Grid>
             
             <Grid item xs={8}>
-                <Button variant="contained" onClick={reserve}>Add box</Button>
-            </Grid>
-        </Grid>
-
-        <Grid container spacing={2}>
-
-            <Grid item xs={6}>
-                <h1>Add device to box</h1>
+                <Button variant="contained" onClick={addBox}>Add box</Button>
             </Grid>
         </Grid>
 
     </div>
+   
     )
 }
 
-export default GettingStarted;
+export default addBox;
